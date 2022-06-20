def4a1ab56487f927ceffda054879f1a25539211
Hanssoy <86066543+Hanssoy@users.noreply.github.com>
2022-06-21 02:11:39 +0900

    Update settings.py

--- firstPj/firstPj/settings.py
+++ firstPj/firstPj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-n*a-v+#8zbb+oxp=0a(2hui7&4rt=i(b^h=wg)k3^-7=h!e)dx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,7 +119,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'firstapp/static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
